4d578b5536b4613f8b79aeadc429f98d4d3502bf
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2014-10-27 01:58:39 -0400

    Removed unused code snippet in landing.js Renamed controllers. Added a grid for the username/password.

--- app/landing/landing.js
+++ app/landing/landing.js
@@ -23,23 +23,23 @@
         $scope.signUpModal = function() {
             var modalInstance = $modal.open({
                 templateUrl: 'singUpModalContent.html',
-                controller: 'SignUp',
-                size: 'sm',
+                controller: 'SignUpController',
+                size: 'md',
                 resolve: {}
             });
         };
         $scope.loginModal = function() {
             var modalInstance = $modal.open({
                 templateUrl: 'loginModalContent.html',
-                controller: 'LogIn',
-                size: 'sm',
+                controller: 'LogInController',
+                size: 'md',
                 resolve: {}
             });
         };
     }]);
 
     //LogIn
-    userControllers.controller("LogIn", ["$scope", "$modalInstance", function($scope, $modalInstance) {
+    userControllers.controller("LogInController", ["$scope", "$modalInstance", function($scope, $modalInstance) {
 
         $scope.ok = function() {
             $modalInstance.close();
@@ -50,7 +50,7 @@
         };
 
     }]);
-    userControllers.controller("SignUp", ["$scope", "$modalInstance", function($scope, $modalInstance) {
+    userControllers.controller("SignUpController", ["$scope", "$modalInstance", function($scope, $modalInstance) {
 
         $scope.ok = function() {
             $modalInstance.close();
@@ -63,50 +63,4 @@
     }]);
 
 
-}(this));
-
-(function(that) {
-    angular.module('landingSite').controller('ModalDemoCtrl', function($scope, $modal, $log) {
-
-        $scope.items = ['item1', 'item2', 'item3'];
-
-        $scope.open = function(size) {
-
-            var modalInstance = $modal.open({
-                templateUrl: 'myModalContent.html',
-                controller: 'ModalInstanceCtrl',
-                size: size,
-                resolve: {
-                    items: function() {
-                        return $scope.items;
-                    }
-                }
-            });
-
-            modalInstance.result.then(function(selectedItem) {
-                $scope.selected = selectedItem;
-            }, function() {
-                $log.info('Modal dismissed at: ' + new Date());
-            });
-        };
-    });
-
-    // Please note that $modalInstance represents a modal window (instance) dependency.
-    // It is not the same as the $modal service used above.
-
-    angular.module('landingSite').controller('ModalInstanceCtrl', function($scope, $modalInstance, items) {
-
-        $scope.items = items;
-        $scope.selected = {
-            item: $scope.items[0]
-        };
-
-        $scope.ok = function() {
-            $modalInstance.close($scope.selected.item);
-        };
-
-        $scope.cancel = function() {
-            $modalInstance.dismiss('cancel');
-        };
-    });
 }(this));
